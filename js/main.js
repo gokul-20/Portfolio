3aeae471adeab0af546ca73835ff6a34f4394928
gokul-20 <gokul04200326@gmail.com>
2024-01-08 04:20:48 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const showMenu = (toogleId, navId)=>{
+    const toogle =document.getElementById(toogleId)
+    nav = document.getElementById(navId);
+    if(toogle && nav){
+        toogle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
+        });
+    }
+
+}
+showMenu('nav_toggle','nav_menu')
+
+const navLink=document.querySelectorAll('.nav_link')
+navLink.forEach(n=> n.classList.remove('active'))
+
+function linkAction(){
+    navLink.forEach(n=> n.classList.remove('active'))
+    this.classList.add('active')
+    const nav_menu=document.getElementById('nav_menu')
+    nav_menu.classList.remove('show')
+}
+navLink.forEach(n=>n.addEventListener('click', linkAction))
+
+function showPdfList() {
+    const pdfList = document.getElementById('pdfList');
+    if (pdfList) {
+        pdfList.style.display = (pdfList.style.display === 'none') ? 'block' : 'none';
+    }
+}
